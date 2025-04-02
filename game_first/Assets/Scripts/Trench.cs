27cd2ae378ec335731270ae8935788406461110d
YurLea <vsailevyrui@gmail.com>
2025-04-02 18:30:10 +0300

    Класс Trench подготовлен для реализации

--- game_first/Assets/Scripts/Trench.cs
+++ game_first/Assets/Scripts/Trench.cs
@@ -1,7 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
 namespace Game
 {
-    public class Trench
+    public class Trench : MonoBehaviour
     {
+        private GameObject[] trenchSegments;
+        private float segmentHalfLength;
+        private List<GameObject> currentSegments;
+        private GameObject randomSegment;
 
+        private void Start(){
+
+        }
+
+        public void ChangeTrenchState(Vector3 playerPosition){
+            
+        }
     }
 }
