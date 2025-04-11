907421cb44333885625ea24f2807b516a9e9bf2b
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2025-04-12 00:46:23 +0500

    Сделана первая версия вызова метода появления врагов при подлете к соответствующему сегменту туннеля.

--- game_first/Assets/Scripts/EnemySpawn.cs
+++ game_first/Assets/Scripts/EnemySpawn.cs
@@ -1,6 +1,26 @@
+using System;
 using UnityEngine;
 
 public class EnemySpawn: MonoBehaviour
 {
+    private int spawnedAfter;
+    private void Awake()
+    {
+        Trench.OnGenerateContinuationOfTrench += CountFragmentsToSpawn;
+    }
 
+    private void CountFragmentsToSpawn(Trench.TrenchState state)
+    {
+        spawnedAfter++;
+        if (spawnedAfter == 5)
+        {
+            SpawnEnemy();
+            Trench.OnGenerateContinuationOfTrench -= CountFragmentsToSpawn;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        // логика появления врагов
+    }
 }
