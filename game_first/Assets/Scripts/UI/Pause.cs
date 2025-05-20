dcc81bb7fe132a07909d1dc0ab1f959828ed63cb
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2025-05-20 20:26:17 +0500

    Пауза сохраняет TimeScale.

--- game_first/Assets/Scripts/UI/Pause.cs
+++ game_first/Assets/Scripts/UI/Pause.cs
@@ -8,6 +8,7 @@ public class Pause : MonoBehaviour
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject stat;
     private bool paused;
+    private float timeScale;
 
     [SerializeField] private PostProcessVolume postProcessVolume;
     private ChromaticAberration chromaticAberrationEffect;
@@ -25,7 +26,8 @@ public class Pause : MonoBehaviour
     private void PauseOnAction(InputAction.CallbackContext obj)
     {
         paused = !paused;
-        Time.timeScale = paused ? 0 : 1;
+        if (Time.timeScale != 0) timeScale = Time.timeScale;
+        Time.timeScale = paused ? 0 : timeScale;
         if(pauseMenu != null) pauseMenu.SetActive(paused);
         if(stat != null) stat.SetActive(!paused);
         if (chromaticAberrationEffect != null)
