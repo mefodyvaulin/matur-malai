af3460d44d5ac8f25e6f066418b012d43f4cabcc
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2025-06-12 01:08:29 +0500

    music change to streaming type

--- game_first/Assets/Scripts/MainMenuUI/ChangeSceneTo.cs
+++ game_first/Assets/Scripts/MainMenuUI/ChangeSceneTo.cs
@@ -1,22 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ChangeSceneTo : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-    
+
     public void StartGameScene(bool education)
     {
         AudioListener.pause = false;
-
-        if (sceneName == "MefodiyCube") GameModel.ResetModel();
+        GameModel.ResetModel();
         Cursor.visible = sceneName != "MefodiyCube";
         Cursor.lockState = sceneName != "MefodiyCube"? CursorLockMode.None: CursorLockMode.Locked;
-        if (sceneName == "MainMenuScene")
-        {
-            GameModel.ResetEducate();
-            Statistic.SaveStat();
-        }
+        if (sceneName == "MainMenuScene") Statistic.SaveStat();
         SceneManager.LoadSceneAsync(sceneName);
         GameModel.isEducation = education;
         Time.timeScale = 1;
